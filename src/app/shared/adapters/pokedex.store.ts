b1cf6c4c973731f7d3dcd382dd8687a81ecbdf40
Mark Kenneth Santos <mark.santos@ffufm.com>
2024-04-03 15:07:29 +0800

    fix: wrong subject name

--- src/app/shared/adapters/pokedex.store.ts
+++ src/app/shared/adapters/pokedex.store.ts
@@ -5,13 +5,13 @@ import { PokedexState } from '../../core/ports/driven/pokedex.state';
 
 @Injectable()
 export class PokedexStore implements PokedexState {
-  tenants$ = new BehaviorSubject<Pokedex[]>([]);
+  pokemons$ = new BehaviorSubject<Pokedex[]>([]);
 
   set(data: Pokedex[]): void {
-    this.tenants$.next(data);
+    this.pokemons$.next(data);
   }
 
   selectAll(): Observable<Pokedex[]> {
-    return this.tenants$.asObservable();
+    return this.pokemons$.asObservable();
   }
 }
